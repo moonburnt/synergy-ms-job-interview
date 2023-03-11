b3f606804c7a7ec876f5b82fd0e52ecb34cb9b0d
moonburnt <moonburnt@disroot.org>
2023-03-11 16:12:50 +0300

    add /users/{id}/add_deposit/ and /users/{id}/detailed/ endpoints

--- referal/users/serializers.py
+++ referal/users/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from users.models import ReferalUserModel
+from decimal import Decimal
 import logging
 
 log = logging.getLogger(__name__)
 
 
-class ReferalUserSerializer(serializers.ModelSerializer):
+class SmallReferalUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReferalUserModel
         fields = (
@@ -13,3 +14,28 @@ class ReferalUserSerializer(serializers.ModelSerializer):
             "referal_lvl"
         )
         read_only_fields = fields
+
+class ReferalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReferalUserModel
+        fields = "__all__"
+
+
+class AddDepositReferalUserSerializer(serializers.ModelSerializer):
+    deposit = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        min_value=Decimal(0.00),
+    )
+
+    class Meta:
+        model = ReferalUserModel
+        fields = (
+            "referal_id",
+            "referal_lvl",
+            "deposit",
+        )
+        read_only_fields = (
+            "referal_id",
+            "referal_lvl",
+        )
--- referal/users/views.py
+++ referal/users/views.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from . import serializers
 from . import models
 
 
 class ReferalUserViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = serializers.ReferalUserSerializer
+    serializer_class = serializers.SmallReferalUserSerializer
     # Since we didn't get any specific instructions about that.
     # On production, it may be better to use at least IsAuthenticated
     permission_classes = [AllowAny]
@@ -13,3 +15,53 @@ class ReferalUserViewSet(viewsets.ReadOnlyModelViewSet):
     # Since we decided to don't override the default ID, tinkering with lookup
     # field to retrieve specific model's instanced by referal_id instead of pk
     lookup_field = "referal_id"
+
+    def get_serializer_class(self):
+        if self.action == "add_deposit":
+            return serializers.AddDepositReferalUserSerializer
+        elif self.action == "detailed":
+            return serializers.ReferalUserSerializer
+        else:
+            return self.serializer_class
+
+    # We could allow adding deposits via PATCH requests, but that may be a bit
+    # more troublesome to implement
+    @action(
+        methods = ("post",),
+        detail=True,
+    )
+    def add_deposit(self, request, **kwargs) -> Response:
+        """Add deposit for to the specific user's wallet"""
+
+        user: models.ReferalUserModel = self.get_object()
+
+        serialized = self.get_serializer(
+            instance = user,
+            data = request.data,
+        )
+
+        serialized.is_valid(raise_exception=True)
+        serialized.save()
+
+        return Response(
+            data = serialized.data,
+            status = status.HTTP_200_OK,
+        )
+
+    @action(
+        methods = ("get",),
+        detail=True,
+    )
+    def detailed(self, request, **kwargs) -> Response:
+        """Get detailed info about user"""
+
+        user: models.ReferalUserModel = self.get_object()
+
+        serialized = self.get_serializer(
+            instance = user,
+        )
+
+        return Response(
+            data = serialized.data,
+            status = status.HTTP_200_OK,
+        )
